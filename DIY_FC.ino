a2c1828787d8b811d9763fbbd45f788ed7073080
Ruhaan2707 <99601220+Ruhaan2707@users.noreply.github.com>
2023-05-03 23:51:12 +0530

    Update DIY_FC.ino

--- DIY_FC.ino
+++ DIY_FC.ino
@@ -18,6 +18,7 @@ https://github.dev/nickrehm/dRehmFlight/
 #include "src/SBUS/SBUS.h"
 #include <TinyGPS++.h>
 #include "src/MadgwickAHRS/src/MadgwickAHRS.h"
+#include <Servo.h>
 
 /*                       parameters taken directly from betaflight using resource command                         */
 #define BEEPER_1 PC5
@@ -62,6 +63,11 @@ https://github.dev/nickrehm/dRehmFlight/
 #define OSD_CS_1 PB12
 #define GYRO_EXTI_1 PC4
 #define GYRO_CS_1 PA4
+#define MIN_PULSE_LENGTH 1000 // Minimum pulse length in µs
+#define MAX_PULSE_LENGTH 2000 // Maximum pulse length in µs
+// ---------------------------------------------------------------------------
+Servo motA, motB, motC, motD;
+char data;
 
 /*                                       paramters from dump                                      */
 int vbat_scale = 110;
@@ -964,6 +970,14 @@ void printCoreTemp()
   Serial.println(coreTemp);
 }
 
+void displayInstructions()
+{  
+    Serial.println("READY - PLEASE SEND INSTRUCTIONS AS FOLLOWING :");
+    Serial.println("\t0 : Send min throttle");
+    Serial.println("\t1 : Send max throttle");
+    Serial.println("\t2 : Run test function\n");
+}
+
 void setup()
 {
   pinMode(GYRO_CS_1, OUTPUT);
@@ -987,7 +1001,12 @@ void setup()
       delay(1000);
     }
   }
-
+    motA.attach(PB6, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+    motB.attach(PB7, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+    motC.attach(PB8, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+    motD.attach(PB9, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+    
+    displayInstructions();
   // calculate_IMU_error();
   calibrateRadioData();
 }
@@ -1342,6 +1361,56 @@ void checkFailsafe()
     m6_command_scaled = 0;
   }
 }
+
+void commandMotors();
+{
+  if (Serial.available()) {
+        data = Serial.read();
+
+        switch (data) {
+            // 0
+            case 48 : Serial.println("Sending minimum throttle");
+                      motA.writeMicroseconds(MIN_PULSE_LENGTH);
+                      motB.writeMicroseconds(MIN_PULSE_LENGTH);
+                      motC.writeMicroseconds(MIN_PULSE_LENGTH);
+                      motD.writeMicroseconds(MIN_PULSE_LENGTH);
+            break;
+
+            // 1
+            case 49 : Serial.println("Sending maximum throttle");
+                      motA.writeMicroseconds(MAX_PULSE_LENGTH);
+                      motB.writeMicroseconds(MAX_PULSE_LENGTH);
+                      motC.writeMicroseconds(MAX_PULSE_LENGTH);
+                      motD.writeMicroseconds(MAX_PULSE_LENGTH);
+            break;
+
+            // 2
+            case 50 : Serial.print("Running test in 3");
+                      delay(1000);
+                      Serial.print(" 2");
+                      delay(1000);
+                      Serial.println(" 1...");
+                      delay(1000);
+                      for (int i = MIN_PULSE_LENGTH; i <= MAX_PULSE_LENGTH; i += 5) {
+                      Serial.print("Pulse length = ");
+                      Serial.println(i);
+        
+                     motA.writeMicroseconds(i);
+                     motB.writeMicroseconds(i);
+                     motC.writeMicroseconds(i);
+                     motD.writeMicroseconds(i);
+        
+                    delay(200);
+                }
+
+                    Serial.println("STOP");
+                    motA.writeMicroseconds(MIN_PULSE_LENGTH);
+                    motB.writeMicroseconds(MIN_PULSE_LENGTH);
+                    motC.writeMicroseconds(MIN_PULSE_LENGTH);
+                    motD.writeMicroseconds(MIN_PULSE_LENGTH);
+    break;
+}
+
 void loop()
 {
   prev_time = current_time;
